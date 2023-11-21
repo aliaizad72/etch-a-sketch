45c865bb832027cd8982b7edfcbfbf0ec21d7ba4
Ali Aizad <aliaizad72@gmail.com>
2023-11-21 13:49:35 +0800

    Able to create grids that are not 16x16

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
-/* 
-SELECT sketch container variable
-CREATE empty div
-APPEND to sketch container
-REPEAT 16 times
-
-SELECT all the divs inside the sketch container
-CREATE empty div
-APPEND to each div 16 times
-*/
-
-const sketchContainer = document.querySelector('#sketch-container');
-
-for (i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.setAttribute('class', 'rows');
-    if (i === 15) {
-        row.style.borderBottom = `1px solid black`;
-    }
-
-    for(j = 0; j < 16; j++) {
-        const column = document.createElement('div');
-        column.setAttribute('class', 'column');
-        column.addEventListener('mouseover', paintItBlack);
-
-        row.appendChild(column);
-        if(j === 15) {
-            column.style.borderRight = '0';
+function createGrid (numRows, numColumns) {
+    const sketchContainer = document.querySelector('#sketch-container');
+    for (i = 0; i < numRows; i++) {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'rows');
+        row.style.height = calculateDimensions(numRows);
+        if (i === numRows-1) {
+            row.style.borderBottom = `1px solid black`;
         }
-    }
     
-    sketchContainer.appendChild(row);
+        for(j = 0; j < numColumns; j++) {
+            const column = document.createElement('div');
+            column.setAttribute('class', 'column');
+            column.style.width = calculateDimensions(numColumns);
+            column.addEventListener('mouseover', paintItBlack);
+            row.appendChild(column);
+            if(j === numColumns-1) {
+                column.style.borderRight = '0';
+            }
+        }
+        
+        sketchContainer.appendChild(row);
+    }
+}
+
+function calculateDimensions (numSquare) {
+    return `${600/numSquare}px`
 }
 
 function paintItBlack(event) {
     event.target.classList.add('painted');
 }
+
